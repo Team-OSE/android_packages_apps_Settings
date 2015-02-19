AOKP/packages_apps_Settings

Clean up code after ScreenType FWB commit
Change-Id: I07874c68802674695877c6bd0329c8973dd1c653

src/com/android/settings/ButtonSettings.java
@@ -38,6 +38,8 @@ import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.WindowManagerGlobal;
 
+import com.android.internal.util.cm.ScreenType;
+
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
@@ -334,7 +336,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             boolean hasNavBar = WindowManagerGlobal.getWindowManagerService().hasNavigationBar()
                     || forceNavbar;
 
-            if (!Utils.isPhone(getActivity())) {
+            if (!ScreenType.isPhone(getActivity())) {
                 mNavigationPreferencesCat.removePreference(mNavigationBarLeftPref);
             }
 
@@ -528,7 +530,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         if (preference == mSwapVolumeButtons) {
             int value = mSwapVolumeButtons.isChecked()
-                    ? (Utils.isTablet(getActivity()) ? 2 : 1) : 0;
+                    ? (ScreenType.isTablet(getActivity()) ? 2 : 1) : 0;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.SWAP_VOLUME_KEYS_ON_ROTATION, value);
         } else if (preference == mDisableNavigationKeys) {

src/com/android/settings/profiles/ProfilesList.java
@@ -29,9 +29,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ListView;
 
+import com.android.internal.util.cm.ScreenType;
+
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
 
 public class ProfilesList extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -53,7 +54,7 @@ public class ProfilesList extends SettingsPreferenceFragment implements
         refreshList();
 
         // On tablet devices remove the padding
-        if (Utils.isTablet(getActivity())) {
+        if (ScreenType.isTablet(getActivity())) {
             getListView().setPadding(0, 0, 0, 0);
         }
     }

src/com/android/settings/profiles/ProfilesSettings.java
@@ -45,11 +45,12 @@ import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import com.android.internal.util.cm.ScreenType;
+
 import com.android.settings.R;
 import com.android.settings.SettingsActivity;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.SubSettings;
-import com.android.settings.Utils;
 import com.android.settings.cyanogenmod.BaseSystemSettingSwitchBar;
 
 public class ProfilesSettings extends SettingsPreferenceFragment
@@ -136,7 +137,7 @@ public class ProfilesSettings extends SettingsPreferenceFragment
         updateProfilesEnabledState();
 
         // If running on a phone, remove padding around tabs
-        if (!Utils.isTablet(getActivity())) {
+        if (!ScreenType.isTablet(getActivity())) {
             mContainer.setPadding(0, 0, 0, 0);
         }
     }

src/com/android/settings/voicewakeup/VoiceWakeupSettings.java
@@ -37,10 +37,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.internal.util.cm.ScreenType;
+
 import com.android.settings.R;
 import com.android.settings.SettingsActivity;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
 import com.android.settings.cyanogenmod.BaseSystemSettingSwitchBar;
 import com.android.settings.cyanogenmod.ShortcutPickHelper;
 
@@ -142,7 +143,7 @@ public class VoiceWakeupSettings extends SettingsPreferenceFragment
         }
 
         // If running on a phone, remove padding around tabs
-        if (!Utils.isTablet(getActivity())) {
+        if (!ScreenType.isTablet(getActivity())) {
             mContainer.setPadding(0, 0, 0, 0);
         }
     }

Team-OSE/android_packages_apps_Settings

Conflicts:
	src/com/android/settings/ButtonSettings.java
	src/com/android/settings/profiles/ProfilesSettings.java
	src/com/android/settings/voicewakeup/VoiceWakeupSettings.java

src/com/android/settings/ButtonSettings.java
@@ -43,6 +43,9 @@ import android.view.WindowManagerGlobal;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
+
+import com.android.internal.util.ose.ScreenType;
+
 import com.android.settings.ose.ButtonBacklightBrightness;
 
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -386,7 +389,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             boolean hasNavBar = WindowManagerGlobal.getWindowManagerService().hasNavigationBar()
                     || forceNavbar;
 
-            if (!Utils.isPhone(context)) {
+            if (!ScreenType.isPhone(context)) {
                 result.put(KEY_NAVIGATION_BAR_LEFT, CATEGORY_NAVBAR);
             }
 
@@ -582,7 +585,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         if (preference == mSwapVolumeButtons) {
             int value = mSwapVolumeButtons.isChecked()
-                    ? (Utils.isTablet(getActivity()) ? 2 : 1) : 0;
+                    ? (ScreenType.isTablet(getActivity()) ? 2 : 1) : 0;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.SWAP_VOLUME_KEYS_ON_ROTATION, value);
         } else if (preference == mDisableNavigationKeys) {

src/com/android/settings/profiles/ProfilesList.java
@@ -29,9 +29,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ListView;
 
+import com.android.internal.util.ose.ScreenType;
+
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
 
 public class ProfilesList extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -53,7 +54,7 @@ public class ProfilesList extends SettingsPreferenceFragment implements
         refreshList();
 
         // On tablet devices remove the padding
-        if (Utils.isTablet(getActivity())) {
+        if (ScreenType.isTablet(getActivity())) {
             getListView().setPadding(0, 0, 0, 0);
         }
     }

src/com/android/settings/profiles/ProfilesSettings.java
@@ -45,11 +45,12 @@ import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import com.android.internal.util.ose.ScreenType;
+
 import com.android.settings.R;
 import com.android.settings.SettingsActivity;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.SubSettings;
-import com.android.settings.Utils;
 import com.android.settings.ose.BaseSystemSettingSwitchBar;
 
 public class ProfilesSettings extends SettingsPreferenceFragment
@@ -136,7 +137,7 @@ public class ProfilesSettings extends SettingsPreferenceFragment
         updateProfilesEnabledState();
 
         // If running on a phone, remove padding around tabs
-        if (!Utils.isTablet(getActivity())) {
+        if (!ScreenType.isTablet(getActivity())) {
             mContainer.setPadding(0, 0, 0, 0);
         }
     }

src/com/android/settings/voicewakeup/VoiceWakeupSettings.java
@@ -37,10 +37,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.internal.util.ose.ScreenType;
+
 import com.android.settings.R;
 import com.android.settings.SettingsActivity;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
 import com.android.settings.ose.BaseSystemSettingSwitchBar;
 import com.android.settings.ose.ShortcutPickHelper;
 
@@ -136,7 +137,7 @@ public class VoiceWakeupSettings extends SettingsPreferenceFragment implements
         }
 
         // If running on a phone, remove padding around tabs
-        if (!Utils.isTablet(getActivity())) {
+        if (!ScreenType.isTablet(getActivity())) {
             mContainer.setPadding(0, 0, 0, 0);
         }
     }